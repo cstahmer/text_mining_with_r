077e8f237090ba1222997112cebe256188311614
Carl G Stahmer <cstahmer@gmail.com>
2016-04-15 06:41:29 -0700

    added better comments and file output command

--- text_mining/process_single_file.R
+++ text_mining/process_single_file.R
@@ -1,59 +1,68 @@
-#load needed library
+# load needed library
 library(tm)
 
 # set working directory
-setwd("~/Documents/rstudio_workspace/digitalmethods/text_mining/TextAnalysisWithR")
+setwd("~/Documents/rstudio_workspace/digitalmethods/text_mining")
 
-#Assign a file path
+# Assign a file path
 filePath <- "data/plainText/melville.txt"
 
-#load the file
+# load the file
 text.v <- scan(filePath, what="character", sep="\n")
-#text.v <- scan("http://www.gutenberg.org/cache/epub/2701/pg2701.txt", what="character", sep="\n")
+# text.v <- scan("http://www.gutenberg.org/cache/epub/2701/pg2701.txt", what="character", sep="\n")
 
-#convert everything to lower case
+# convert everything to lower case
 text.lower.v <- tolower(text.v)
 
-#remove stopwords
+# remove stopwords
 text.lower.v <- removeWords(text.lower.v, stopwords("english"))
 
-#tokenize to words in a list
+# tokenize to words in a list
 text.words.l <- strsplit(text.lower.v, "\\W")
 
-#convert my list to a vector
+# convert my list to a vector
 text.word.v <- unlist(text.words.l)
 
-#return vector of slices in list that aren't blank
+# return vector of slices in list that aren't blank
 noblanks.v <- which(text.word.v!="")
 
-#rewrite text vector with only non-blank slices
+# rewrite text vector with only non-blank slices
 text.word.v <- text.word.v[noblanks.v]
 
+# get the length of the words vector
+# tells us how many words we have
 totalwords.i <- length(text.word.v)
 
-#get the number of times a specific word appears in the text
-wordcount.single.i <- length(text.word.v[which(text.word.v=="the")])
+# tells us how many words we have
+print(totalwords.i)
 
-#calculate the percentage frequency of tested word
+# get the number of times a specific word appears in the text
+wordcount.single.i <- length(text.word.v[which(text.word.v=="must")])
+
+# tell us how many times our word of interest appears
+print(wordcount.single.i)
+
+# calculate the percentage frequency of tested word
 word.percent.occurence.i <- wordcount.single.i / totalwords.i
 
-#get a word frequency table
+# tell us the percentage occurrence
+print(word.percent.occurence.i)
+
+# get a word frequency table for all words
 text.freqs.t <- table(text.word.v)
 
-#sort the frequency table
+# sort the frequency table
 sorted.text.freqs.t <- sort(text.freqs.t , decreasing=TRUE)
 
+# output the first 500 words
+print(sorted.text.freqs.t[1:500])
 
-totalwords.i
-#wordcount.single
-#word.percent.occurence.i
-
-sorted.text.freqs.t[1:500]
-
+# plot the word frequencies
 plot(sorted.text.freqs.t[1:500])
 
-write.table(sorted.text.freqs.t, file = "/Users/cstahmer/Desktop/not_i_top_500.csv", sep = ",", col.names = NA,
-            qmethod = "double")
+# save the results to a csv file
+write.csv(sorted.text.freqs.t, file = "/Users/cstahmer/textmining/word_freq_500.csv")
 
+# mark the end of execution
 print("Bye Bye!")
 
